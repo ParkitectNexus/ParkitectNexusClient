45956a285aae9157e9c7112a22d8bac07f5f408b
Michael Pollind <mpollind@gmail.com>
2016-02-29 14:14:00 -0800

    fixed app image

--- AppImage/Packager.sh
+++ AppImage/Packager.sh
@@ -1,12 +1,13 @@
 APP=ParkitectNexus
 MONO_URL=https://kojipkgs.fedoraproject.org//packages/mono/4.2.2/3.fc24/x86_64/mono-core-4.2.2-3.fc24.x86_64.rpm
-
+GTK_SHARP=http://download.mono-project.com/repo/centos/g/gtk-sharp2/gtk-sharp2-2.12.26-0.x86_64.rpm
+GLIB_SHARP=http://download.mono-project.com/repo/centos/g/gtk-sharp2/glib-sharp2-2.12.26-0.x86_64.rpm
 
 mkdir -p ./$APP/$APP.AppDir/usr/bin
 mkdir -p ./$APP/$APP.AppDir/usr/opt
-mkdir -p ./$APP/$APP.AppDir/usr/opt/nexus
 
-cp -R ./../src/ParkitectNexus.Client.Linux/bin/Release/* ./$APP/$APP.AppDir/usr/opt/nexus
+xbuild /p:Configuration=Release ./../src/ParkitectNexus.Client.Linux/ParkitectNexus.Client.Linux.csproj
+cp -R ./../src/ParkitectNexus.Client.Linux/bin/Release/* ./$APP/$APP.AppDir/usr/bin
 cp parkitectnexus.desktop $APP/$APP.AppDir/.
 cp parkitectnexus $APP/$APP.AppDir/usr/bin/.
 cp parkitectnexus.wrapper $APP/$APP.AppDir/usr/bin/
@@ -15,15 +16,21 @@ chmod a+x $APP/$APP.AppDir/usr/bin/parkitectnexus
 cd ./$APP
 
 wget -c --trust-server-names "$MONO_URL"
+wget -c --trust-server-names "$GTK_SHARP"
+wget -c --trust-server-names "$GLIB_SHARP"
 
 cd $APP.AppDir/
-rpm2cpio ../*.rpm . | cpio -idmv
+rpm2cpio ../mono-core*.rpm . | cpio -idmv
+rpm2cpio ../gtk-sharp2*.rpm . | cpio -idmv
+rpm2cpio ../glib-sharp2*.rpm . | cpio -idmv
 
 
 wget -c "https://github.com/probonopd/AppImageKit/releases/download/5/AppRun" # (64-bit)
 chmod a+x ./AppRun
 
-cp ./usr/opt/nexus/parkitectnexus_logo.png parkitectnexus.png
+mv etc/ usr/
+
+cp ./usr/bin/parkitectnexus_logo.png parkitectnexus.png
 
 
 # Add desktop integration
---
+++ AppImage/test.sh
@@ -0,0 +1,4 @@
+cd out
+git clone https://github.com/probonopd/AppImageKit.git
+
+wget https://download.fedoraproject.org/pub/fedora/linux/releases/23/Workstation/x86_64/iso/Fedora-Live-Workstation-x86_64-23-10.iso
