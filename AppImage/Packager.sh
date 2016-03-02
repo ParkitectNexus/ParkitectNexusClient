b90c1c35f140d404a13a00fc2474539888c07b29
Michael Pollind <mpollind@gmail.com>
2016-03-02 10:14:38 -0800

    fixes to package script

--- AppImage/Packager.sh
+++ AppImage/Packager.sh
@@ -4,7 +4,7 @@ MONO_CORE=http://download.mono-project.com/repo/centos/m/mono-core/mono-core-4.2
 MONO_LOCALE=https://kojipkgs.fedoraproject.org//packages/mono/4.0.5/3.fc23/x86_64/mono-locale-extras-4.0.5-3.fc23.x86_64.rpm
 GTK_SHARP=http://download.mono-project.com/repo/centos/g/gtk-sharp2/gtk-sharp2-2.12.26-0.x86_64.rpm
 GLIB_SHARP=http://download.mono-project.com/repo/centos/g/gtk-sharp2/glib-sharp2-2.12.26-0.x86_64.rpm
-GDK_PIX=https://kojipkgs.fedoraproject.org//packages/gdk-pixbuf2/2.33.2/2.fc24/x86_64/gdk-pixbuf2-2.33.2-2.fc24.x86_64.rpm
+#GDK_PIX=https://kojipkgs.fedoraproject.org//packages/gdk-pixbuf2/2.33.2/2.fc24/x86_64/gdk-pixbuf2-2.33.2-2.fc24.x86_64.rpm
 #GTK2=https://kojipkgs.fedoraproject.org//packages/gtk2/2.24.29/2.fc24/x86_64/gtk2-2.24.29-2.fc24.x86_64.rpm
 
 #create APP directory
@@ -33,7 +33,7 @@ wget -c --trust-server-names "$MONO_CORE"
 wget -c --trust-server-names "$GTK_SHARP"
 wget -c --trust-server-names "$GLIB_SHARP"
 wget -c --trust-server-names "$MONO_LOCALE"
-wget -c --trust-server-names "$GDK_PIX"
+#wget -c --trust-server-names "$GDK_PIX"
 #wget -c --trust-server-names "$GTK2"
 
 cd $APP.AppDir/
@@ -44,7 +44,7 @@ rpm2cpio ../mono-basic*.rpm . | cpio -idmv
 rpm2cpio ../gtk-sharp2*.rpm . | cpio -idmv
 rpm2cpio ../glib-sharp2*.x86_64.rpm . | cpio -idmv
 rpm2cpio ../mono-locale*.x86_64.rpm . | cpio -idmv
-rpm2cpio ../gdk-pixbuf*.x86_64.rpm . | cpio -idmv
+#rpm2cpio ../gdk-pixbuf*.x86_64.rpm . | cpio -idmv
 #rpm2cpio ../gtk2*.rpm . | cpio -idmv
 
 #grab AppRun
